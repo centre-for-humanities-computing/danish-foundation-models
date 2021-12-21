1733ccaf7b02564732f755f6bde141e1d6a47c77
hjb <hjb@kmd.dk>
2021-12-21 13:29:03 +0100

    load datasets  and pretraining config

--- src/dfm/data/load.py
+++ src/dfm/data/load.py
@@ -1,11 +1,18 @@
 """Dataset loading script for Danish Foundation Models
 """
+from typing import List
+from datasets import load_dataset
 
 
 def load_dataset(datasets: List[str]):
     # parse dataset names
     # load required dataset
+    # interleave if necessary
     # apply relevant filters
-    # train test validation split
+    # train test, validation split
     # (dont to shuffling, tokenization etc. here)
-    pass
+
+    # dummy dataset for testing
+    ds = load_dataset("DDSC/reddit-da")
+    ds.shuffle(seed=42)
+    ds_dict = ds.train_test_split(test_size=0.1)
