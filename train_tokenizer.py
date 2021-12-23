dae1dc7dc83ef0d83e32501a27dc0dc0003a628f
Kenneth Enevoldsen (UCloud) <kennethcenevoldsen@gmail.com>
2021-12-23 18:11:40 +0100

    added script for training tokenizer

---
+++ train_tokenizer.py
@@ -0,0 +1,13 @@
+from dfm.data import load_dfm_dataset
+from dfm.tokenizers import train_tokenizer, TokenizerConfig
+from wasabi import msg
+ds = load_dfm_dataset("tokenization")
+t_config = TokenizerConfig(tokenizer_type = "unigram",
+    vocab_size = 32000,
+    lower_case = False,
+    sentence_piece = True,
+    add_prefix_space = True,
+    byte_level =  False)
+
+msg.info("Started training tokenizer")
+train_tokenizer(corpus = ds, config=t_config)
