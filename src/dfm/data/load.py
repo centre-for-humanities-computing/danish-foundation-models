0c782b86b1d456446a8774aa685e59829a47c461
hjb <hjb@kmd.dk>
2022-01-10 10:47:57 +0100

    load dataset and return train, test, val dictionaries

--- src/dfm/data/load.py
+++ src/dfm/data/load.py
@@ -1,18 +1,42 @@
 """Dataset loading script for Danish Foundation Models
 """
-from typing import List
+from typing import Optional
 from datasets import load_dataset
+from datasets import DatasetDict
 
 
-def load_dataset(datasets: List[str]):
-    # parse dataset names
-    # load required dataset
-    # interleave if necessary
-    # apply relevant filters
-    # train test, validation split
-    # (dont to shuffling, tokenization etc. here)
+def main():
+    ds = dfm_load_dataset("DDSC/reddit-da")
+    print("DONE")
 
-    # dummy dataset for testing
-    ds = load_dataset("DDSC/reddit-da")
-    ds.shuffle(seed=42)
-    ds_dict = ds.train_test_split(test_size=0.1)
+
+def dfm_load_dataset(dataset: str):
+    """loads a single dataset from the Hugging Face Datasets hub.
+
+    Args:
+        dataset (str): string for Hugging Face dataset.
+
+    Returns:
+        DatasetDict: Dataset with train, test and validation split.
+    """
+
+    ds = load_dataset(dataset)
+
+    if "validation" and "test" not in ds:
+        train_test = ds["train"].train_test_split(test_size=0.1)
+        # Split the 10% test + valid in half test, half valid
+        test_valid = train_test["test"].train_test_split(test_size=0.5)
+        # gather everyone if you want to have a single DatasetDict
+        ds = DatasetDict(
+            {
+                "train": train_test["train"],
+                "test": test_valid["test"],
+                "val": test_valid["train"],
+            }
+        )
+
+    return ds
+
+
+if __name__ == "__main__":
+    main()
