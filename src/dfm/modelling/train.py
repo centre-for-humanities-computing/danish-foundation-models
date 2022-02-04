929c24f690fceb72576f5c968f632c5ba8202414
hjb <hjb@kmd.dk>
2022-02-04 09:00:22 +0100

    created IaC structure

--- src/dfm/modelling/train.py
+++ src/dfm/modelling/train.py
@@ -5,12 +5,14 @@ CMD: train.py --path_to_config_file
 """
 
 from dataclasses import dataclass, field
+import json
 from typing import List, Optional
 
 import wandb
 from transformers import (
     AutoConfig,
     AutoModelForMaskedLM,
+    AutoModelForPreTraining,
     Trainer,
     TrainingArguments,
     AutoTokenizer,
@@ -25,14 +27,56 @@ from dfm.modelling.model_types import MODEL_TYPES
 
 
 def main():
+    d = DataArguments.from_json("tests/test_configs/pretrain_config.json")
     trainer = DFMTrainer(
-        model_type="autoencoding",
-        model_name="microsoft/deberta-v3-base",
         pretraining_config_path="tests/test_configs/pretrain_config.json",
-        dataset_names=["DDSC/reddit-da", "DDSC/angry-tweets"],
-        interleave_probabilities=[0.5, 0.5],
     )
     trainer.train()
+
+
+# TODO:
+# create configs for models (training + model)
+# create collator for T5
+# remove variables and make it more IaC
+@dataclass
+class DataArguments(object):
+    dataset_names: List[str]
+    interleave: bool
+    interleave_probabilities: List[float]
+
+    @classmethod
+    def from_json(cls, config_path: str):
+        file = json.load(open(config_path))
+        keys = cls.__dataclass_fields__.keys()
+        json_data = file["data_arguments"]
+        normal_json_data = {key: json_data[key] for key in json_data if key in keys}
+        anormal_json_data = {
+            key: json_data[key] for key in json_data if key not in keys
+        }
+        tmp = cls(**normal_json_data)
+        for anormal_key in anormal_json_data:
+            setattr(tmp, anormal_key, anormal_json_data[anormal_key])
+        return tmp
+
+
+@dataclass
+class ModelArguments(object):
+    model_type: str
+    model_name: str
+
+    @classmethod
+    def from_json(cls, config_path: str):
+        file = json.load(open(config_path))
+        keys = cls.__dataclass_fields__.keys()
+        json_data = file["model_arguments"]
+        normal_json_data = {key: json_data[key] for key in json_data if key in keys}
+        anormal_json_data = {
+            key: json_data[key] for key in json_data if key not in keys
+        }
+        tmp = cls(**normal_json_data)
+        for anormal_key in anormal_json_data:
+            setattr(tmp, anormal_key, anormal_json_data[anormal_key])
+        return tmp
 
 
 class DFMTrainer:
@@ -40,17 +84,14 @@ class DFMTrainer:
 
     def __init__(
         self,
-        model_type: str,
         pretraining_config_path: str,
-        dataset_names: List[str],
+        model_type: Optional[str] = None,
+        dataset_names: Optional[List[str]] = None,
         model_name: Optional[str] = None,
         interleave_probabilities: Optional[List[float]] = [0.5, 0.5],
+        mlm_probability: int = 0.15,
     ) -> None:
         """
-        # TODO:
-            # create configs for models (training + model)
-            # test 3 modeller t5, deberta v2, gpt
-
         Training function with Weights & Biases logging.
 
         Args:
@@ -59,26 +100,29 @@ class DFMTrainer:
             datasets (list(str)): List of dataset names from the Hugging Face hub.
             interleave_probabilities (List[float]): how to weight datasets in interleave
         """
-        if model_type not in MODEL_TYPES:
-            raise ValueError(f"Invalid model type. Choose one from: {MODEL_TYPES}")
-        self.model_type = model_type
-
-        if model_name is None:
-            self.model_name = self.model_type
-        else:
-            self.model_name = model_name
+        # Configuration arguments
         self.pretraining_config_path = pretraining_config_path
-        self.dataset_names = dataset_names
-        self.interleave_probabilities = interleave_probabilities
+        self.training_args = read_json(self.pretraining_config_path)[
+            "training_arguments"
+        ]
+        self.data_args = DataArguments.from_json(self.pretraining_config_path)
+        self.model_args = ModelArguments.from_json(self.pretraining_config_path)
+
+        self.dataset_names = self.data_args.dataset_names
+        self.interleave = self.data_args.interleave
+        self.interleave_probabilities = self.data_args.interleave_probabilities
+
+        self.model_type = self.model_args.model_type
+        self.model_name = self.model_args.model_name
+
+        if self.model_type not in MODEL_TYPES:
+            raise ValueError(f"Invalid model type. Choose one from: {MODEL_TYPES}")
 
     def train(
         self,
     ):
 
         # wandb.login()
-
-        # Configuration
-        pretrain_config = read_json(self.pretraining_config_path)
 
         HF_config = AutoConfig.from_pretrained(self.model_name)
         tokenizer = AutoTokenizer.from_pretrained(
@@ -99,26 +143,32 @@ class DFMTrainer:
             eval_datasets, probabilities=self.interleave_probabilities
         )
 
-        # Load model
+        # Data collator
         if self.model_type == "autoenconding":
             # Data Collator
             data_collator = DataCollatorForLanguageModeling(
-                tokenizer=tokenizer, mlm_probability=0.15
+                tokenizer=tokenizer, mlm_probability=self.mlm_probability
             )
-            mdl = AutoModelForPretraining.from_config(HF_config)
 
         elif self.model_type == "seq-to-seq":
-            data_collator = DataCollatorForLanguageModeling(
-                tokenizer=tokenizer, mlm_probability=0.15
+            # data_collator = DataCollatorForLanguageModeling(
+            #     tokenizer=tokenizer, mlm_probability=0.15
+            # )
+            # model = AutoModelForMaskedLM.from_config(HF_config)
+            raise ValueError(
+                f"Sorry. Model type: {self.model_type} has not yet been implemented."
             )
-            mdl = AutoModelForMaskedLM.from_config(HF_config)
 
+        # Load model
+        model = AutoModelForPreTraining.from_config(HF_config)
+
+        # Training args
         training_args = TrainingArguments(
-            f"models/{mdl.name_or_path}", **pretrain_config
+            f"models/{model.name_or_path}", **pretrain_config
         )
 
         trainer = Trainer(
-            model=mdl,
+            model=model,
             args=training_args,
             train_dataset=train_datasets,
             eval_dataset=eval_datasets,
