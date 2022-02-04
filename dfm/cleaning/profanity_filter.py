b90ddfdf9d540ad49e855d91f1cf1ccd0bd2d23b
mollerhoj <mollerhoj3@gmail.com>
2022-02-04 12:20:38 -0600

    add tests and expand wordlist

--- dfm/cleaning/profanity_filter.py
+++ dfm/cleaning/profanity_filter.py
@@ -5,94 +5,227 @@ Authors:
     Jens Dahl Møllerhøj
 """
 
+from unicodedata import normalize
 from typing import Iterable
+import re
 
 class ProfanityFilter:
     """
     Danish implementation of a profanity filter.
     """
     def __init__(self):
-        self.danish_pornograpic_words = [
-            "anal",
-            "anus",
-            "røv",
-            "æsler",
-            "røvhul",
-            "røvhuller",
-            "dyrisk",
-            "bestialitet",
-            "tæve",
-            "tæver",
-            "blodig",
-            "blowjob",
-            "bryster",
-            "sprække",
-            "klitoris",
-            "lort",
-            "kusse",
-            "dildo",
-            "dildoer",
-            "ejakulere",
-            "ejakulerede",
-            "ejakulerer",
-            "sædafgang",
-            "ejakulation",
-            "svans",
-            "fanden",
-            "fucked",
-            "fucker",
+        self.danish_swear_words = set([
             "skide",
             "forbandede",
             "pokkers",
+            "æsler",
             "helvede",
-            "hore",
-            "liderlig",
-            "skamlæber",
+        ])
+
+        self.danish_pornograpic_words = set([
+            "amatør",
+            "amatører",
+            "anal",
+            "anus",
+            "babes",
+            "bdsm",
             "begær",
+            "bestialitet",
+            "blodig",
+            "blowjob",
+            "bordel",
+            "bordeller",
+            "bryster",
+            "bøsse",
+            "bøssefilm",
+            "c-skål",
+            "damer",
+            "dating",
+            "dildo",
+            "dildoer",
+            "dildomaskine",
+            "dyrisk",
+            "ejakulation",
+            "ejakulere",
+            "ejakulerede",
+            "ejakulerer",
+            "elskerinde",
+            "endetarm",
+            "erotik",
+            "erotisk",
+            "erotiske",
+            "escort",
+            "escortpige",
+            "escortpiger",
+            "escortpigerne",
+            "fanden",
+            "fisse",
+            "fisser",
+            "fræk",
+            "frække",
+            "frækt",
+            "fucked",
+            "fucker",
+            "gangbang",
+            "gay",
+            "hardcore",
+            "hentai",
+            "homo",
+            "hore",
+            "intim",
+            "intime",
+            "kinky",
+            "klitoris",
+            "kneppe",
+            "kusse",
+            "kvinder",
+            "latex",
+            "latino",
+            "lesbisk",
+            "liderlig",
+            "liderlige",
+            "lort",
+            "lorte",
+            "luder",
             "masochist",
-            "onanere",
+            "massage",
+            "massageescort",
+            "massageklinik",
+            "massagen",
+            "massagepige",
+            "massagepiger",
+            "massagepigerne",
+            "milf",
             "nigger",
             "niggere",
+            "nøgenbillede",
+            "nøgenbilleder",
+            "nøgenbillederne",
+            "nøgne",
+            "onanere",
             "orgasme",
             "orgasmer",
+            "patter",
             "pecker",
             "penis",
+            "piger",
+            "pigesex",
+            "pik",
             "pis",
+            "pisse",
             "pisser",
             "pisses",
-            "pisse af",
+            "porn",
             "porno",
+            "porno-casting",
+            "pornofilm",
             "pornografi",
-            "pik",
-            "fisse",
-            "voldtage",
-            "voldtægt"
-            "endetarm",
+            "pornostar",
+            "pornostjerne",
+            "pornostjernen",
+            "pornostjerner",
+            "prostitueret",
+            "røv",
+            "røvhul",
+            "røvhuller",
             "sadist",
-            "sæd",
-            "skider",
-            "lorte",
-            "tøs",
+            "samleje",
+            "sex",
+            "sexcam",
+            "sexdating",
+            "sexdatingsites",
+            "sexfilm",
+            "sexfoto",
+            "sexhistorier",
+            "sexparadis",
+            "sexshop",
+            "sexstillinger",
+            "sexvideo",
+            "sexvideoer",
+            "sexvideoerne",
+            "sexvideoen",
+            "sexy",
+            "shemale",
+            "shemales",
+            "sjofelhed",
             "sjofelheder",
+            "sjofelhederne",
+            "skamlæber",
+            "skider",
+            "sluger",
+            "sm",
+            "spanking",
+            "sprække",
+            "sprøjteorgasme",
+            "sprøjteorgasmer",
+            "sprøjteorgasmen",
+            "sprøjteorgasmerne",
+            "strip",
+            "svans",
+            "swinger",
+            "swingerdating",
+            "swingerklub",
+            "sæd",
+            "sædafgang",
+            "tantra",
+            "telefonsex",
             "testikel",
+            "thai",
+            "thaimassage",
+            "thaipiger",
+            "tranny",
+            "tæve",
+            "tæver",
+            "tøs",
+            "tøser",
+            "urin",
             "vagina",
+            "vaginaen",
             "viagra",
+            "viagraen",
+            "voldtage",
+            "voldtager",
+            "voldtægt"
             "vulva",
-            "luder",
+            "webcam",
+            "webcam-chat",
             "x-bedømt",
-            "xxx"]
+            "xxx"])
 
-    def __call__(self, docs: Iterable[str]) -> Generator:
+    def __call__(self, docs: Iterable[str]) -> Iterable[str]:
         """Applies profanity filter
         """
+        for raw_doc in docs:
+            # normalize
+            doc = raw_doc
+            doc = normalize("NFKC", doc)
+            doc = re.sub(r"[\.\,\:\;\!\?\(\)\[\]\{\}]", " ", doc)
+            doc = re.sub(" +", " ", doc)
 
-        # normalize
+            # lowercase
+            doc = doc.lower()
 
-        # count occurrences of known profanities in document
+            # count occurrences of known profanities in document
+            tokens = doc.split(' ')
+            words = set(tokens)
+            profanities = []
 
-        # flag document based on heuristic
+            for word in words:
+                if word in self.danish_pornograpic_words:
+                    profanities.append(word)
 
-        # print possible violating documents
+            percentage = (100.0 * len(profanities))/ len(tokens)
 
-        return docs
+            # flag document based on heuristic
+            if len(profanities) < 3:
+                # yield if not filtered
+                yield raw_doc
+            else:
+                # print possible violating documents
+                print("--------------")
+                print("profanities: ", profanities)
+                print("percentage: ", percentage)
+                if (percentage > 4):
+                    print(words - self.danish_pornograpic_words)
+                print("--------------")
 
--- tests/cleaning/profanity_filter_test.py
+++ tests/cleaning/profanity_filter_test.py
@@ -1,17 +1,27 @@
 from dfm.cleaning import ProfanityFilter
 import pytest
 
+from dfm.data.load import (
+    load_mc4
+)
+
 class TestProfanityFilter:
     def test_filters_inappropriate_text(self):
         profanity_filter = ProfanityFilter()
         
         assert (
-                profanity_filter(["Penis"]) == []
+                list(profanity_filter(["Penis"])) == []
         )
 
     def test_does_not_filter_appropriate_text(self):
         profanity_filter = ProfanityFilter()
         
         assert (
-                profanity_filter(["Hello"]) == ["Hello"]
+                list(profanity_filter(["Hello"])) == ["Hello"]
         )
+
+    def test_filter_on_mc4(self):
+        profanity_filter = ProfanityFilter()
+        ds = load_mc4(streaming = True)
+        texts = (d['text'] for d in ds['train'])
+        list(profanity_filter(texts))
