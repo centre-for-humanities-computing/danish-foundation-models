0ae6a0d7c3013d556465d0eadbc2326abe490ff2
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-02-09 11:22:27 +0100

    added examination scripts of quality filter

---
+++ src/examine_quality_filter.py/dagw_clean.py
@@ -0,0 +1,53 @@
+"""
+Extract description of using the quality filter on DAGW
+
+To run profile the code:
+
+.. code::
+    pip install snakeviz
+
+    # run script with cProfile
+    python -m cProfile -o tmp.prof src/dagw_clean.py
+
+    # view result
+    snakeviz tmp.prof
+"""
+
+import sys
+import ndjson
+import time
+
+sys.path.append("..")
+sys.path.append(".")
+
+
+from dfm.cleaning import QualityFilter
+from dfm.data.load import load_dfm_dataset
+
+
+s = time.time()
+
+ds = load_dfm_dataset("dagw", streaming=False)
+
+ds = ds.remove_columns(["LICENSE"])
+
+batch_size = 1024 * 4
+
+
+def q_filter(batch, batch_size=batch_size):
+    qf = QualityFilter()
+    batch["filtered"] = list(qf.describe_filter(batch["text"], batch_size=batch_size))
+    return batch
+
+
+ds = ds.map(q_filter, batched=True, batch_size=batch_size, num_proc=12)
+
+ds = ds.remove_columns(["text"])
+
+with open("DAGW_filter2.ndjson", "w") as f:
+    writer = ndjson.writer(f, ensure_ascii=False)
+
+    for sample in ds:
+        writer.writerow(sample)
+
+print("Done, took:", time.time() - s)
---
+++ src/examine_quality_filter.py/plot_quality.py
@@ -0,0 +1,70 @@
+"""
+A script for plotting and examining the effect of the quality filter.
+"""
+
+import pandas as pd
+import os
+
+
+f_path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(f_path, "..", "..", "DAGW_filter2.ndjson")
+
+df = pd.read_json(path, lines=True)
+df["Counts"] = df.index
+
+mapping_dict = {
+    "retsinformationdk": "Legal",
+    "skat": "Legal",
+    "retspraksis": "Legal",
+    "hest": "Social Media",
+    "cc": "Web",
+    "adl": "Wiki & Books",
+    "botxt": "Other",
+    "danavis": "News",
+    "dannet": "dannet",
+    "depbank": "Other",
+    "ep": "Conversation",
+    "ft": "Conversation",
+    "gutenberg": "Wiki & Books",
+    "jvj": "Wiki & Books",
+    "naat": "Conversation",
+    "opensub": "Conversation",
+    "relig": "Wiki & Books",
+    "spont": "Conversation",
+    "synne": "Other",
+    "tv2r": "News",
+    "wiki": "Wiki & Books",
+    "wikibooks": "Wiki & Books",
+    "wikisource": "Wiki & Books",
+}
+
+
+mapping_dict2 = {
+    "retsinformationdk": "Retsinformation",
+    "skat": "Skat.dk",
+    "retspraksis": "H-Sø/retspraksis",
+    "hest": "Hestenettet",
+    "cc": "CommonCrawl",
+    "adl": "Danish Litterature",
+    "botxt": "Botxt (Bornholmsk?)",
+    "danavis": "DanAvis",
+    "dannet": "dannet",  # WHAT ARE YOU!
+    "depbank": "Danish Dependendency treebank",
+    "ep": "European Parlament",
+    "ft": "Folketinget",
+    "gutenberg": "Gutenberg",
+    "jvj": "Johannes V. Jensen",
+    "naat": "NAAT",
+    "opensub": "Open subtitles",
+    "relig": "Religiuous texts",
+    "spont": "spontaneaus speech",
+    "synne": "Synnejysk forening/Sønderjysk",
+    "tv2r": "TV2 Regionerne",
+    "wiki": "Wikipedia",
+    "wikibooks": "WikiBooks",
+    "wikisource": "WikiSource",
+}
+
+# df[["source", "filtered", "Counts"]].groupby(["source", "filtered"]).count()
+df["source_cat"] = df["source"].apply(lambda x: mapping_dict[x])
+df[["source_cat", "filtered", "Counts"]].groupby(["source_cat", "filtered"]).count()
