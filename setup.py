83e44a2667e818f9de876708f75a0872d97209c8
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-03-04 10:35:16 +0100

    Restructure package to follow the recommendations
    The recommendation from ionel:
    https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
    and pytest:
    https://docs.pytest.org/en/6.2.x/goodpractices.html

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def setup_package():
+    setup(
+        packages=find_packages("src"),
+        package_dir={"": "src"},
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
