368877800e47e2841bd3fd9739330d16ceef1356
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-03-04 12:03:53 +0100

    removed os from setup

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
 from setuptools import find_packages
-import os
-
 
 def setup_package():
     setup(
         packages=find_packages(
             "src",
             exclude=[
-                os.path.join("src", "application"),
-                os.path.join("src", "application", "*"),
+                "src/application",
+                "src/application/*",
             ],
         ),
         package_dir={"": "src"},
