74be8828bc775db3401db566afd038ad59218f6b
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-03-10 15:28:49 +0100

    added batch to utils

---
+++ src/dfm/utils.py
@@ -0,0 +1,26 @@
+from typing import Iterable
+from itertools import islice
+
+
+def batch(dataset: Iterable, batch_size: int) -> Iterable:
+    """Creates batches from an iterable.
+
+    Args:
+        dataset (Iterable): Your dataset you want to batch given as an iterable (e.g. a
+            list).
+        batch_size (int): Your desired batch size
+
+    Returns:
+        Iterable: An iterable of tuples of size equal to batch_size.
+
+    Example:
+        >>> batches = batch([1,2, 3, 4, 5], 2)
+        >>> print(list(batches))
+        [(1, 2), (3, 4), (5,)]
+    """
+    iterable_dataset = iter(dataset)
+    while True:
+        chunk = tuple(islice(iterable_dataset, batch_size))
+        if not chunk:
+            break
+        yield chunk
