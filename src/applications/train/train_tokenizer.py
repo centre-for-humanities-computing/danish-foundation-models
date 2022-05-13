6390e4926cc58e65dfa403ff0ce8cd801ccbf6a9
Kenneth Enevoldsen (UCloud) <kennethcenevoldsen@gmail.com>
2022-05-13 21:21:53 +0200

    added column normalization to load_dataset

--- src/applications/train/train_tokenizer.py
+++ src/applications/train/train_tokenizer.py
@@ -1,14 +1,25 @@
 """
 Trains the tokenizer on the DFM corpus
 """
-from dfm.data import load_dfm_dataset
-from dfm.tokenizers import train_tokenizer, TokenizerConfig
 from wasabi import msg
+from datasets import interleave_datasets
+
+from dfm.data import load_dfm_dataset
+from dfm.dfm_tokenizers import train_tokenizer, TokenizerConfig
+from dfm.data.load_datasets import load_danews, load_dagw_dfm, load_hopetwitter, load_nat
+
+columns_to_keep = ["text", "source"]
+danews = load_danews(columns_to_keep=columns_to_keep)["train"]
+dagw_dfm = load_dagw_dfm(columns_to_keep=columns_to_keep)["train"]
+hopetwitter = load_hopetwitter(columns_to_keep = columns_to_keep)["train"]
+nat = load_nat(columns_to_keep=columns_to_keep)["train"]
+dataset = interleave_datasets([danews, dagw_dfm, hopetwitter, nat], probabilities=[0.10, 0.20, 0.20, 0.50])
+
+len(list(dataset.take(30)))
+colnames = next(iter(danews)).keys()
 
 
-ds = load_dfm_dataset("tokenization")
-ds = ds.take(30)
-
+dataset.remove_columns('timestamp')
 
 t_config = TokenizerConfig(
     tokenizer_type = "unigram",
--- src/dfm/data/load_datasets.py
+++ src/dfm/data/load_datasets.py
@@ -1,14 +1,16 @@
 
-from typing import Union
+from typing import Union, Iterable, List, Optional
 import os
 from pathlib import Path
+from functools import partial
 
 from datasets import (
     load_dataset,
     interleave_datasets,
     DatasetDict,
     IterableDatasetDict,
-    load_from_disk
+    load_from_disk,
+    interleave_datasets,
 )
 
 HOPETWITTER_PATH = Path("/work") / "twitter_cleaned"
@@ -17,13 +19,36 @@ DANEWS_PATH = Path("/work") / "hope-infomedia_cleaned"
 NAT_PATH = Path("/work") / "netarkivet-cleaned"
 
 
-def load_hopetwitter(path_to_hopetwitter: Union[str, Path]=HOPETWITTER_PATH, **kwargs) -> IterableDatasetDict:
+
+def __add_source(example, source):
+    example["source"] = source
+    return example
+
+def keep_columns(
+    dataset: Union[IterableDatasetDict, DatasetDict], 
+    columns_to_keep: List[str]
+) -> Union[IterableDatasetDict, DatasetDict]:
+    subset = dataset[list(dataset.keys())[0]]
+    sample = next(iter(subset))
+    col_to_remove = [c_name for c_name in sample.keys() if c_name not in columns_to_keep]
+    
+    return dataset.remove_columns(col_to_remove)
+
+
+
+def load_hopetwitter(
+        path_to_hopetwitter: Union[str, Path]=HOPETWITTER_PATH, 
+        columns_to_keep: Optional[List[str]]=None, 
+        **kwargs
+    ) -> IterableDatasetDict:
     """
     Loads HopeTwitter.
 
     Args:
-        path_to_hopetwitter (Union[str, Path, None]): path to the HopeTwitter.
+        path_to_hopetwitter (Union[str, Path, None], optional): path to the HopeTwitter.
             Defaults to "/work/twitter_cleaned".
+        columns_to_keep (Optional[List[str]], optional): Columns to keep. Default to None
+            in which case all columns are kept.
         kwargs: arguments to be passed forward to load_dataset
     
     Returns:
@@ -34,20 +59,31 @@ def load_hopetwitter(path_to_hopetwitter: Union[str, Path]=HOPETWITTER_PATH, **k
     test = path_to_hopetwitter / "twitter_da_stopwords_2019-01-01_2021-04-30_v1.0.0_test.jsonl"
     val = path_to_hopetwitter / "twitter_da_stopwords_2019-01-01_2021-04-30_v1.0.0_val.jsonl"
     train = path_to_hopetwitter / "twitter_da_stopwords_2019-01-01_2021-04-30_filtered_v1.0.0_train.jsonl"
-    hopetwitter = load_dataset("json", data_files={"train": [str(train)], "test": [str(test)], "validation": [str(val)]}, streaming=True)
+    data_files = {"train": [str(train)], "test": [str(test)], "validation": [str(val)]}
+    hopetwitter = load_dataset("json", data_files=data_files, streaming=True)
+
+    _add_source = partial(__add_source, source = "hopetwitter")
+    hopetwitter = hopetwitter.map(_add_source)
+
+    if columns_to_keep:
+        hopetwitter = keep_columns(hopetwitter, columns_to_keep)
+
     return hopetwitter
 
 
-    HOPETWITTER_PATH = Path("/work") / "twitter_cleaned"
-
-
-def load_dagw_dfm(path_to_dagw: Union[str, Path]=HOPETWITTER_PATH, **kwargs) -> IterableDatasetDict:
+def load_dagw_dfm(
+        path_to_dagw: Union[str, Path]=DAGW_DFM_PATH,
+        columns_to_keep: Optional[List[str]]=None,
+        **kwargs
+    ) -> IterableDatasetDict:
     """
     Loads DAGW_{DFM}.
 
     Args:
-        path_to_dagw (Union[str, Path]): path to the DAGW_DFM.
+        path_to_dagw (Union[str, Path], optional): path to the DAGW_DFM.
             Defaults to /work/dagw-clean.
+        columns_to_keep (Optional[List[str]], optional): Columns to keep. Default to None
+            in which case all columns are kept.
         kwargs: arguments to be passed forward to load_dataset
     
     Returns:
@@ -58,18 +94,30 @@ def load_dagw_dfm(path_to_dagw: Union[str, Path]=HOPETWITTER_PATH, **kwargs) -> 
     test = path_to_dagw / "dagw_reddit_v1.0.0_test.jsonl"
     val = path_to_dagw / "dagw_reddit_v1.0.0_val.jsonl"
     train = path_to_dagw / "dagw_reddit_filtered_v1.0.0_train.jsonl"
-    dagw = load_dataset("json", data_files={"train": [str(train)], "test": [str(test)], "validation": [str(val)]}, streaming=True)
+    data_files = {"train": [str(train)], "test": [str(test)], "validation": [str(val)]}
+    dagw = load_dataset("json", data_files=data_files, streaming=True)
+    
+    next(iter(dagw["train"]))
+    
+    if columns_to_keep:
+        dagw = keep_columns(dagw, columns_to_keep)
+
     return dagw
 
 
-
-def load_danews(path_to_danews: Union[str, Path]=DANEWS_PATH, **kwargs) -> IterableDatasetDict:
+def load_danews(
+        path_to_danews: Union[str, Path]=DANEWS_PATH,
+        columns_to_keep: Optional[List[str]]=None,
+        **kwargs
+    ) -> IterableDatasetDict:
     """
     Loads DaNews.
 
     Args:
-        path_to_danews (Union[str, Path]): path to the DaNews dataset.
+        path_to_danews (Union[str, Path], optional): path to the DaNews dataset.
             Defaults to /work/hope-infomedia_cleaned
+        columns_to_keep (Optional[List[str]], optional): Columns to keep. Default to None
+            in which case all columns are kept.
         kwargs: arguments to be passed forward to load_dataset
     
     Returns:
@@ -77,32 +125,58 @@ def load_danews(path_to_danews: Union[str, Path]=DANEWS_PATH, **kwargs) -> Itera
     """
     path_to_danews = Path(path_to_danews)
 
-    test = path_to_danews / ""
-    val = path_to_danews / ""
-    train = path_to_danews / ""
-    danews = load_dataset("json", data_files={"train": [str(train)], "test": [str(test)], "validation": [str(val)]}, streaming=True)
+    test = path_to_danews / "infomedia_2000-2021_v1.0.0_test.jsonl"
+    val = path_to_danews / "infomedia_2000-2021_v1.0.0_val.jsonl"
+    train = path_to_danews / "infomedia_2000-2021_filtered_v1.0.0_train.jsonl"
+    data_files = {"train": [str(train)], "test": [str(test)], "validation": [str(val)]}
+    danews = load_dataset("json", data_files=data_files, streaming=True)
+    
+    _add_source = partial(__add_source, source = "danews")
+    danews = danews.map(_add_source)
+
+    if columns_to_keep:
+        danews = keep_columns(danews, columns_to_keep)
     return danews
 
 
-def load_nat(path_to_nat: Union[str, Path]=NAT_PATH, years: Iterable[int]=range(2006, 2017), **kwargs) -> IterableDatasetDict:
+def load_nat(
+        path_to_nat: Union[str, Path]=NAT_PATH, 
+        years: Iterable[int]=range(2006, 2017), 
+        probabilities: Optional[List[float]] = None, 
+        seed: Optional[int] = None,
+        columns_to_keep: Optional[List[str]]=None, 
+        **kwargs
+    ) -> IterableDatasetDict:
     """
-    Loads DaNews.
+    Loads NetArkivet Text corpus (NAT).
 
     Args:
-        path_to_nat (Union[str, Path]): path to the NAT dataset.
+        path_to_nat (Union[str, Path], optional): path to the NAT dataset.
             Defaults to /work/netarkivet-cleaned.
+        columns_to_keep (Optional[List[str]], optional): Columns to keep. Default to None
+            in which case all columns are kept.
         years (Iterable[int]): A list of years to include.
+        probabilities (Optional[List[float]], optionak): Interleave probabilites of years.
+            Default to None, denoting equal probabilites.
         kwargs: arguments to be passed forward to load_dataset
     
     Returns:
         IterableDatasetDict: A datasets IterableDatasetDict
     """
-    NAT_PATH = Path(NAT_PATH)
+    path_to_nat = Path(path_to_nat)
 
     train_paths = []
     for year in years:
-        train_path = NAT_PATH / f"{year}_deduplicated_filtered.jsonl"
+        train_path = path_to_nat / f"{year}_deduplicated_filtered.jsonl"
         train_paths.append(str(train_path))
-    nat = load_dataset("json", data_files={"train": train_paths}, streaming=True)
-    return nat
+    datasets = [load_dataset("json", data_files={"train": path}, streaming=True, split="train", **kwargs) 
+                for path in train_paths]
+    nat = interleave_datasets(datasets=datasets, probabilities=probabilities, seed=seed)
+    nat = IterableDatasetDict({"train": nat})
+    
+    _add_source = partial(__add_source, source = "nat")
+    nat = nat.map(_add_source)
 
+    if columns_to_keep:
+        nat = keep_columns(nat, columns_to_keep)
+    return nat
