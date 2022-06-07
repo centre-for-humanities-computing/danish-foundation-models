b04da777a993a662960629c9cfbe26b51ea79e5a
Kenneth Enevoldsen (UCloud) <kennethcenevoldsen@gmail.com>
2022-06-07 16:48:12 +0200

    updated script

--- src/applications/evaluation/pseudo_perplexity.py
+++ src/applications/evaluation/pseudo_perplexity.py
@@ -1,6 +1,8 @@
 """
 script for calculating pseudo perplexity in accordance with
 https://arxiv.org/pdf/1910.14659.pdf
+
+Note: this script could be optimized more by sampling instead of calculating the full document
 """
 
 from typing import Dict, List
@@ -8,26 +10,17 @@ from transformers import (
     AutoModel,
     AutoTokenizer,
     FlaxAutoModelForMaskedLM,
-    AutoModelForMaskedLM,
+    AutoModelForMaskedLM
 )
 from scipy.special import expit
 import numpy as np
 
-model = FlaxAutoModelForMaskedLM.from_pretrained("KennethEnevoldsen/dfm-bert-base")
-tokenizer = AutoTokenizer.from_pretrained("KennethEnevoldsen/dfm-bert-base")
-
-# model = AutoModel.from_pretrained("KennethEnevoldsen/dfm-bert-base", from_flax=True)
-# pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-# 110617344
-
-# pytorch_total_params = sum(p.numel() for p in model.parameters())
-# 110617344
-
-
+from dfm.data.load_datasets import load_danews, load_dagw_dfm, load_hopetwitter
+import torch
 def ppl(
-    text: str, model: AutoModelForMaskedLM, tokenizer: AutoTokenizer
+    text: str, model: AutoModelForMaskedLM, tokenizer: AutoTokenizer, batch_size=64,
 ) -> Dict[str, float]:
-    inputs = tokenizer(text, return_tensors="np")
+    inputs = tokenizer(text, return_tensors="np", truncation=True, max_length=512)
     special_tokens = set(tokenizer.all_special_ids)
     special_tokens = np.isin(inputs["input_ids"][0], tokenizer.all_special_ids)
     non_special_tokens = np.logical_not(special_tokens)
@@ -39,55 +32,88 @@ def ppl(
     arr = batch[:, non_special_tokens]
     arr[np.identity(n_non_special_tokens, dtype="bool")] = tokenizer.mask_token_id
     batch[:, non_special_tokens] = arr
+    attention_mask = np.vstack([inputs["attention_mask"]] * n_non_special_tokens)
+    # removed samples without a mask
+    contains_mask = (batch == tokenizer.mask_token_id).sum(axis=0) == 1
+    batch = batch[:,contains_mask]
+    attention_mask = attention_mask[:,contains_mask]
 
     # recreate input as a masked batch
     inputs_ = {
         "input_ids": batch,
-        "token_type_ids": np.vstack([inputs["token_type_ids"]] * n_non_special_tokens),
-        "attention_mask": np.vstack([inputs["attention_mask"]] * n_non_special_tokens),
+        "attention_mask": attention_mask,
     }
 
-    # forward pass through model
-    token_logits = model(**inputs_).logits
-    # extract logit of gold token
-    gold_token = inputs["input_ids"][0][non_special_tokens]
-    mask_token_index = np.argwhere(inputs_["input_ids"] == tokenizer.mask_token_id)
-    mask_token_logits = token_logits[mask_token_index[:, 0], mask_token_index[:, 1], :]
-    gold_token_logits = mask_token_logits[np.arange(n_non_special_tokens), gold_token]
+    ppl = 0
+    n_samples = inputs_["input_ids"].shape[0]
+    for i in range(0, n_samples, batch_size):
+        batch_input = {
+            "input_ids": inputs_["input_ids"][:, i:i+batch_size],
+            "attention_mask": inputs_["attention_mask"][:, i:i+batch_size],
+        }
+        if hasattr(model, "device"):
+            batch_input_ = {
+                "input_ids": torch.from_numpy(batch_input["input_ids"]).to(model.device),
+                "attention_mask": torch.from_numpy(batch_input["attention_mask"]).to(model.device),
+                }
+        else:
+            batch_input_ = batch_input
+        # forward pass through model
+        token_logits = model(**batch_input_).logits
 
-    # calculate PPL in accordance with https://arxiv.org/pdf/1910.14659.pdf
-    pll = np.sum(np.log(expit(gold_token_logits)))
-    return {"ppl": pll, "n_tokens": len(gold_token_logits)}
+        if isinstance(token_logits, torch.Tensor):
+            token_logits = token_logits.cpu().detach().numpy()
 
+        # extract logit of gold token
+        gold_token = inputs["input_ids"][0][non_special_tokens]
+        mask_token_index = np.argwhere(batch_input["input_ids"] == tokenizer.mask_token_id)
+        mask_token_logits = token_logits[mask_token_index[:, 0], mask_token_index[:, 1], :]
+        gold_token_logits = mask_token_logits[np.arange(mask_token_logits.shape[0]), gold_token[i:i+batch_size]]
 
-text = "Jeg er så glad for min cykel."
-ppl(text, model, tokenizer)
-
+        # calculate PPL in accordance with https://arxiv.org/pdf/1910.14659.pdf
+        ppl += np.sum(np.log(expit(gold_token_logits)))
+    return {"ppl": ppl, "n_tokens": len(gold_token_logits)}
 
 def pppl(
     texts: List[str], model: AutoModelForMaskedLM, tokenizer: AutoTokenizer
 ) -> float:
-    ppl_values_ = [ppl(text, model=model, tokenizer=tokenizer) for text in texts]
+    texts_ = [t for t in texts if t]
+    if not texts:
+        raise ValueError("list of texts is empty or contain only empty strings.")
+    ppl_values_ = [ppl(text, model=model, tokenizer=tokenizer) for text in texts_]
     ppl_values, token_lengths = zip(
         *[(ppl_["ppl"], ppl_["n_tokens"]) for ppl_ in ppl_values_]
     )
     return np.exp(-np.sum(ppl_values) / np.sum(token_lengths))
 
-
-texts = [
-    "Jeg er så glad for min cykel.",
-    "Fuck hvor har vejret været godt de her dage!",
+mdls = [
+#    "KennethEnevoldsen/dfm-bert-base",  # 1.4349901347773766
+    "vesteinn/ScandiBERT",  # 1.0686843661248284
 ]
-pppl(texts, model, tokenizer)
+
+datasets = {}
+dagw = load_dagw_dfm()["test"]
+for sample in iter(dagw):
+    if sample["source"] not in datasets:
+        datasets[sample["source"]] = []
+    datasets[sample["source"]].append(sample["text"])
+    
+danews = load_danews()["test"]
+datasets["danews"] = [t["text"] for t in danews]
+hopetwitter = load_hopetwitter()["test"]
+datasets["hopetwitter"] = [t["text"] for t in hopetwitter]
 
 
-text = "Jeg er så glad for min cykel."
-print(ppl(text, model, tokenizer))
-text = "Hej med dig arne. Går det godt?"
-print(ppl(text, model, tokenizer))
-text = "bib bobity bob bob ding ding!"
-print(ppl(text, model, tokenizer))
-text = "bib bobity bob bob ding ding! "*2
-print(ppl(text, model, tokenizer))
-text = "bib bobity bob bob ding ding! "*3
-print(ppl(text, model, tokenizer))
+
+for mdl in mdls:
+    if mdl == "KennethEnevoldsen/dfm-bert-base":
+        model = FlaxAutoModelForMaskedLM.from_pretrained(mdl)
+    else:
+        model = AutoModelForMaskedLM.from_pretrained(mdl)
+        model.cuda()
+    tokenizer = AutoTokenizer.from_pretrained(mdl)
+
+    print(f"Model:{mdl}")
+    for name, texts in datasets.items():
+        pppl_ = pppl(texts, model, tokenizer)
+        print(f"\t{name}, {pppl_}")
