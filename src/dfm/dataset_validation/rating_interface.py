873d654562345fc6e80e0247562c2f2325016b5c
Kenneth Enevoldsen (UCloud) <kennethcenevoldsen@gmail.com>
2022-07-06 11:11:32 +0200

    removed resume from index
    Ideally it should resume from the file, but it does not and can thus cause confusion.

--- src/dfm/dataset_validation/rating_interface.py
+++ src/dfm/dataset_validation/rating_interface.py
@@ -8,11 +8,8 @@ import pandas as pd
 
 
 class ExampleRater:
-    def __init__(self, examples: Iterable, output_path: str, resume_from_index: int):
-        if resume_from_index:
-            self.examples = list(reversed(examples[resume_from_index:]))
-        else:
-            self.examples = list(reversed(examples))
+    def __init__(self, examples: Iterable, output_path: str):
+        self.examples = list(reversed(examples))
 
         self.output_path = output_path
 
