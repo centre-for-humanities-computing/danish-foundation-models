1c17cb0caeae67bdddcd42a6864a3997f9eb9af8
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2022-09-09 16:05:09 +0200

    fix: Removed uninstended import

--- src/dfm/description/__init__.py
+++ src/dfm/description/__init__.py
@@ -3,8 +3,9 @@ from .description_patterns import (  # noqa: F401
     get_gender_name_patterns,
     get_male_gendered_patterns,
     get_muslim_name_patterns,
+    get_negative_word_patterns,
     get_occupation_patterns,
+    get_positive_word_patterns,
     get_religion_patterns,
-    noqa,
 )
 from .match_counter import MatchCounter  # noqa: F401
