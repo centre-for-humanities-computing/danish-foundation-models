804e14ee75851d5f8a1bd167785e5d19cdda846a
Kenneth Enevoldsen (UCloud) <kennethcenevoldsen@gmail.com>
2022-09-14 09:38:53 +0200

    addded run.sh

--- src/applications/netarkivet/cleaning-v2/run.sh
+++ src/applications/netarkivet/cleaning-v2/run.sh
@@ -1,3 +1,22 @@
 # cleaning
+# clean 2006
+python3 danish-foundation-models/src/dfm/cleaning/clean.py \
+    path=/work/netarchive/2006_textcorpus.parquet/*.parquet \
+    save_dir=/work/netarkivet-cleaned/v2 \
+    text_col=content \
+    lang_col=language \
+    num_proc=31 
 
+python3 danish-foundation-models/src/dfm/cleaning/clean.py \
+    path=/work/netarchive/2016_textcorpus.parquet/*.parquet \
+    save_dir=/work/netarkivet-cleaned/v2/2016 \
+    text_col=content \
+    lang_col=language \
+    num_proc=31 
 
+python3 danish-foundation-models/src/dfm/cleaning/clean.py \
+    path=/work/netarchive/2015_textcorpus.parquet/*.parquet \
+    save_dir=/work/netarkivet-cleaned/v2/2015 \
+    text_col=content \
+    lang_col=language \
+    num_proc=31
