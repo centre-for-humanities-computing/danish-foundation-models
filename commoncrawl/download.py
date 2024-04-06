39ae06401aa1d20c84759097a4ee1a93dfb8c6a7
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2024-04-06 14:32:36 +0000

    Refactor CommonCrawl download script

--- commoncrawl/download.py
+++ commoncrawl/download.py
@@ -1,36 +1,74 @@
-import duckdb, glob
+import duckdb
+import glob
+import httpx
+from pathlib import Path
+import argparse
+import asyncio
+import aiofiles
+from tqdm import tqdm
+import os
+import time
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--out_root", type=str, default="/mnt/Common_Crawl/")
+args = parser.parse_args()
 
-crawl50 = "CC-MAIN-2023-50",
 files = glob.glob(
-    f"/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl={crawl50}/subset=warc/*.parquet",
+    f"/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-50/subset=warc/*.parquet",
     recursive=True,
 )
 crawl50 = duckdb.read_parquet(files, hive_partitioning=True)
-crawl40 = "CC-MAIN-2023-50",
 files = glob.glob(
-    f"/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl={crawl40}/subset=warc/*.parquet",
+    f"/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-40/subset=warc/*.parquet",
     recursive=True,
 )
 crawl40 = duckdb.read_parquet(files, hive_partitioning=True)
 
 query = """
-SELECT url_host_registered_domain, 
-       COUNT(*) AS num_pages,
-       SUM(warc_record_length) AS total_bytes,
-       (SUM(CASE WHEN content_languages LIKE '%dan%' THEN 1 ELSE 0 END) * 100.0) / COUNT(*) AS danish_pages_percentage
-FROM (
-    SELECT * FROM crawl50
-    UNION
-    SELECT * FROM crawl40
-) AS combined_crawls
+WITH combined_crawls AS (
+    SELECT * FROM read_parquet('/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-50/subset=warc/*.parquet', hive_partitioning=True)
+    UNION ALL
+    SELECT * FROM read_parquet('/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-40/subset=warc/*.parquet', hive_partitioning=True)
+)
+SELECT STRUCT_PACK(url, 
+       warc_filename,
+       warc_record_offset,
+       warc_record_length,
+       warc_segment) AS crawl_data
+FROM combined_crawls
 WHERE subset = 'warc'
   AND url_host_tld = 'dk'
-GROUP BY url_host_registered_domain
-ORDER BY total_bytes DESC
-"""
+""" 
 
 result = duckdb.sql(query)
-print(result)
+num_rows = result.shape[0]
 
-# duckdb.sql(f'''COPY result TO '{crawl}.csv' (HEADER, DELIMITER ',')''')
+# get all existing files at once, for fast resumption
+existing_files = set(os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk(args.out_root) for f in filenames)
+
+def make_path(row):
+    return os.path.join(args.out_root, f"{row['warc_filename']}-bytes={row['warc_record_offset']}-{row['warc_record_offset'] + row['warc_record_length'] - 1}")
+
+async def fetch_and_save(client, row):
+    path = make_path(row)
+    if path in existing_files:
+        return
+    os.makedirs(os.path.dirname(path), exist_ok=True) # ensure directory exists
+    headers = {
+        'Range': f'bytes={row["warc_record_offset"]}-{row["warc_record_offset"] + row["warc_record_length"] - 1}'
+    }
+    async with client.stream("GET", f"https://data.commoncrawl.org/{row['warc_filename']}", headers=headers, timeout=None) as response:
+        async with aiofiles.open(path, 'wb') as f:
+            async for chunk in response.aiter_bytes():
+                await f.write(chunk)
+
+async def main():
+    async with httpx.AsyncClient(http2=True, limits=httpx.Limits(max_connections=100)) as client:
+        pbar = tqdm(total=num_rows)
+        while batch := result.fetchmany(1000): # process in batches for the sake of memory
+            await asyncio.gather(*[fetch_and_save(client, row[0]) for row in batch])
+            pbar.update(len(batch))
+        pbar.close()
+
+asyncio.run(main())
+
