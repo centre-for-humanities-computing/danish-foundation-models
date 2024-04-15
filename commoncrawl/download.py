bb9b2ef515fe8147047c354c80ba0f39d4c70ca4
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2024-04-15 15:55:28 +0000

    Update file paths and add error handling for downloads

--- commoncrawl/download.py
+++ commoncrawl/download.py
@@ -9,26 +9,28 @@ from tqdm import tqdm
 import os
 import time
 
+from retry_transport import RetryTransport
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--out_root", type=str, default="/mnt/Common_Crawl/")
+parser.add_argument("--out_root", type=str, default="/mnt/usb/Common_Crawl_DK")
 args = parser.parse_args()
 
 files = glob.glob(
-    f"/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-50/subset=warc/*.parquet",
+    f"/mnt/usb/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-50/subset=warc/*.parquet",
     recursive=True,
 )
-crawl50 = duckdb.read_parquet(files, hive_partitioning=True)
+#crawl50 = duckdb.read_parquet(files, hive_partitioning=True)
 files = glob.glob(
-    f"/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-40/subset=warc/*.parquet",
+    f"/mnt/usb/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-40/subset=warc/*.parquet",
     recursive=True,
 )
-crawl40 = duckdb.read_parquet(files, hive_partitioning=True)
+#crawl40 = duckdb.read_parquet(files, hive_partitioning=True)
 
 query = """
 WITH combined_crawls AS (
-    SELECT * FROM read_parquet('/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-50/subset=warc/*.parquet', hive_partitioning=True)
+    SELECT * FROM read_parquet('/mnt/usb/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-50/subset=warc/*.parquet', hive_partitioning=True)
     UNION ALL
-    SELECT * FROM read_parquet('/mnt/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-40/subset=warc/*.parquet', hive_partitioning=True)
+    SELECT * FROM read_parquet('/mnt/usb/Common_Crawl/data.commoncrawl.org/cc-index/**/crawl=CC-MAIN-2023-40/subset=warc/*.parquet', hive_partitioning=True)
 )
 SELECT STRUCT_PACK(url, 
        warc_filename,
@@ -49,7 +51,9 @@ existing_files = set(os.path.join(dirpath,f) for (dirpath, dirnames, filenames) 
 def make_path(row):
     return os.path.join(args.out_root, f"{row['warc_filename']}-bytes={row['warc_record_offset']}-{row['warc_record_offset'] + row['warc_record_length'] - 1}")
 
+failed = False # flag to indicate to stop because downloads are failing
 async def fetch_and_save(client, row):
+    global failed
     path = make_path(row)
     if path in existing_files:
         return
@@ -57,15 +61,25 @@ async def fetch_and_save(client, row):
     headers = {
         'Range': f'bytes={row["warc_record_offset"]}-{row["warc_record_offset"] + row["warc_record_length"] - 1}'
     }
-    async with client.stream("GET", f"https://data.commoncrawl.org/{row['warc_filename']}", headers=headers, timeout=None) as response:
-        async with aiofiles.open(path, 'wb') as f:
-            async for chunk in response.aiter_bytes():
-                await f.write(chunk)
+    response = await client.get(f"https://data.commoncrawl.org/{row['warc_filename']}", headers=headers, timeout=None)
+    if response.status_code != 206:
+        print(f"Failed to download {path}: HTTP {response.status_code}")
+        failed = True
+        return
+    async with aiofiles.open(path, 'wb') as f:
+        await f.write(response.content)
+    if os.path.getsize(path) != row["warc_record_length"]:
+        print(f"Failed to save {path}, size mismatch")
+        failed = True
+        os.remove(path)
+        return
 
 async def main():
-    async with httpx.AsyncClient(http2=True, limits=httpx.Limits(max_connections=100)) as client:
+    async with httpx.AsyncClient(http2=False, limits=httpx.Limits(max_connections=1), transport=RetryTransport(wrapped_transport=httpx.AsyncHTTPTransport())) as client:
         pbar = tqdm(total=num_rows)
         while batch := result.fetchmany(1000): # process in batches for the sake of memory
+            if failed:
+                exit(1)
             await asyncio.gather(*[fetch_and_save(client, row[0]) for row in batch])
             pbar.update(len(batch))
         pbar.close()
