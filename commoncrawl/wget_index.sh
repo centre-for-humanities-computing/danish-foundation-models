eb48eb9072cee70e8f3d86f7d48b2db8f04756ba
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2024-04-29 07:52:58 +0000

    Update Common Crawl index version and fix file download

--- commoncrawl/wget_index.sh
+++ commoncrawl/wget_index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Download the Columnar Index for the Common Crawl WARC files
-CRAWL=CC-MAIN-2022-33
-wget https://data.commoncrawl.org/crawl-data/$CRAWL/cc-index-table.paths.gz
-gzip -d -y cc-index-table.paths.gz
+CRAWL=CC-MAIN-2022-27
+wget https://data.commoncrawl.org/crawl-data/$CRAWL/cc-index-table.paths.gz -O cc-index-table.paths.gz
+gzip -d cc-index-table.paths.gz
 grep 'subset=warc' cc-index-table.paths | wget -c -x -i - --base https://data.commoncrawl.org/
