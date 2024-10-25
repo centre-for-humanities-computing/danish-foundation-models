9a31be0bfaf32e0a9cc617aa1c84e1ad3811402f
kris927b <kristian.smed.noergaard@gmail.com>
2024-10-25 13:34:11 +0200

    Adhering to the desired format

--- data-processing/scripts/dsk/convert_dsk_to_jsonl.py
+++ data-processing/scripts/dsk/convert_dsk_to_jsonl.py
@@ -1,5 +1,25 @@
+"""
+Convert from various file formats (pdf, docx, etc.).
+
+To this format:
+{
+    "id": "...",             # MANDATORY: source-specific identifier
+    "text": "foo",           # MANDATORY: textual content of the document
+    "source": "...",         # MANDATORY: source of the data, such as peS2o, common-crawl, etc.
+    "added": "...",          # OPTIONAL: timestamp we acquired this data (time file was created), specified as YYYY-MM-DDTHH:MM:SS.TIMEZONE e.g 2021-04-13T12:52:46.000Z
+    "created": "..."         # OPTIONAL: timestamp when orig document was created (best-guess if not available), should be specified as a range; "YYYY-MM-DDTHH:MM:SS.TIMEZONE, YYYY-MM-DDTHH:MM:SS.TIMEZONE"
+    "metadata": {            # OPTIONAL: source-specific metadata
+        "sub-source": "...", # OPTIONAL: E.g. "newspaper_ocr"
+        ...
+    }
+}
+"""
+
 import json
 import re
+import gzip
+from dataclasses import asdict, dataclass
+from datetime import datetime
 from pathlib import Path
 from typing import Any
 
@@ -11,46 +31,70 @@ from tqdm import tqdm
 from trafilatura import extract as extract_html_text
 from typer import Typer
 
+
+@dataclass
+class JSONL:
+    id: str
+    text: str
+    source: str
+    added: str
+    created: str
+    metadata: dict[str, Any]
+
+
 APP = Typer(name="2JSONL Converter")
 
 
+def create_JSONL(text: str, source: str, metadata: dict[str, Any]) -> JSONL:
+    id_ = f"{source}-{metadata.get("file_path", "")}".replace(" ", "-")
+    jsonl = JSONL(
+        id=id_,
+        text=text,
+        source=source,
+        added=str(datetime.now()),
+        created=f"{datetime(2000, 1, 1)!s}, {datetime.now()!s}",
+        metadata=metadata,
+    )
+    return jsonl
+
+
 # @APP.command(name="PDF2JSONL", no_args_is_help=True, help="Convert PDF file to JSONL string", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
-def process_pdf(file_path: Path, **kwargs: dict[str, Any]) -> str:
+def process_pdf(file_path: Path, source: str, **kwargs: dict[str, Any]) -> str:
     text = extract_pdf_text(file_path)
     text = re.sub(r"(\n\s)+", "\n", text)
     metadata = {**kwargs, "file_path": str(file_path)}
-    return json.dumps({"text": text, "metadata": metadata}, ensure_ascii=False)
+    return json.dumps(asdict(create_JSONL(text, source, metadata)), ensure_ascii=False)
 
 
 # @APP.command(name="DOCX2JSONL", no_args_is_help=True, help="Convert DOCX file to JSONL string", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
-def process_docx(file_path: Path, **kwargs: dict[str, Any]) -> str:
+def process_docx(file_path: Path, source: str, **kwargs: dict[str, Any]) -> str:
     text = convert_file(file_path, to="plain", format="docx")
     text = re.sub(r"(\n\s)+", "\n", text)
     metadata = {**kwargs, "file_path": str(file_path)}
-    return json.dumps({"text": text, "metadata": metadata}, ensure_ascii=False)
+    return json.dumps(asdict(create_JSONL(text, source, metadata)), ensure_ascii=False)
 
 
 # @APP.command(name="HTML2JSONL", no_args_is_help=True, help="Convert HTML file to JSONL string", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
-def process_html(file_path: Path, **kwargs: dict[str, Any]) -> str:
+def process_html(file_path: Path, source: str, **kwargs: dict[str, Any]) -> str:
     text = extract_html_text(file_path.read_text())
     text = re.sub(r"(\n\s)+", "\n", text)
     metadata = {**kwargs, "file_path": str(file_path)}
-    return json.dumps({"text": text, "metadata": metadata}, ensure_ascii=False)
+    return json.dumps(asdict(create_JSONL(text, source, metadata)), ensure_ascii=False)
 
 
 # @APP.command(name="EPUB2JSONL", no_args_is_help=True, help="Convert EPUB file to JSONL string", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
-def process_epub(file_path: Path, **kwargs: dict[str, Any]) -> str:
+def process_epub(file_path: Path, source: str, **kwargs: dict[str, Any]) -> str:
     text = convert_file(file_path, to="plain", format="epub")
     text = re.sub(r"(\n\s)+", "\n", text)
     metadata = {**kwargs, "file_path": str(file_path)}
-    return json.dumps({"text": text, "metadata": metadata}, ensure_ascii=False)
+    return json.dumps(asdict(create_JSONL(text, source, metadata)), ensure_ascii=False)
 
 
-def process_txt(file_path: Path, **kwargs: dict[str, Any]) -> str:
+def process_txt(file_path: Path, source: str, **kwargs: dict[str, Any]) -> str:
     text = file_path.read_text()
     text = re.sub(r"(\n\s)+", "\n", text)
     metadata = {**kwargs, "file_path": str(file_path)}
-    return json.dumps({"text": text, "metadata": metadata}, ensure_ascii=False)
+    return json.dumps(asdict(create_JSONL(text, source, metadata)), ensure_ascii=False)
 
 
 SUFFIX2METHOD = {
@@ -63,7 +107,7 @@ SUFFIX2METHOD = {
 
 
 @APP.command(name="process_file", help="Process a single file, returning a json string")
-def process_file(file: Path) -> str | None:
+def process_file(file: Path, dsk_client: str) -> str | None:
     if file.is_dir():
         return None
 
@@ -75,7 +119,7 @@ def process_file(file: Path) -> str | None:
         logger.warning(f"Unsupported file type: {suffix} - for file: {file!s}")
         return None
 
-    return method(file, file_type=suffix, filename=name)
+    return method(file, dsk_client, file_type=suffix, filename=name)
 
 
 @APP.command(
@@ -85,17 +129,21 @@ def process_file(file: Path) -> str | None:
 def craw_directory(
     top_level_path: Path,
     output_path: Path,
-    output_name: str = "raw.jsonl",
+    dsk_client: str,
+    output_name: str = "1.jsonl.gz",
+    n_workers: int = 2,
 ):
     files = top_level_path.glob("**/*")
 
-    parallel = Parallel(n_jobs=2, return_as="generator_unordered")
-
-    with (output_path / output_name).open("w+") as out_file:
-        for doc in parallel(delayed(process_file)(file) for file in tqdm(files)):
+    parallel = Parallel(n_jobs=n_workers, return_as="generator_unordered")
+    with gzip.open(output_path / output_name) as out_file:
+        # with (output_path / output_name).open("w+") as out_file:
+        for doc in parallel(
+            delayed(process_file)(file, dsk_client) for file in tqdm(files)
+        ):
             if doc is None:
                 continue
-            out_file.write(f"{doc}\n")
+            out_file.write(f"{doc}\n".encode())
 
 
 if __name__ == "__main__":
